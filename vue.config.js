c643a05b8ce34fca1368fba9b3d13ae0dfe6d924
lixiang <929401425@qq.com>
2020-02-22 22:59:36 +0800

    跨域代理

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,22 @@
+APP_BASE_API = '/api'
+
 module.exports = {
     transpileDependencies: [
         'vue-echarts',
         'resize-detector'
-    ]
+    ],
+    devServer: {
+        port: 80,
+        proxy: {
+            '/admin': {
+                target: 'http://129.211.66.41:8080/admin',
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: {
+                    '^/admin': ''
+                }
+            }
+        }
+
+    }
 }
