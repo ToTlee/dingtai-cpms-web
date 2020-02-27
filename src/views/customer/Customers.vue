76eea0b362fe9ae70c3f90cf3f6068b3894a9f47
lixiang <929401425@qq.com>
2020-02-27 23:23:51 +0800

    修改客户列表信息
    更新数据接口API；
    修改客户列表数据项

--- src/views/customer/Customers.vue
+++ src/views/customer/Customers.vue
@@ -1,29 +1,17 @@
 <!-- 合同管理-客户列表 -->
 <template>
 <el-table :data="data" v-loading="isLoading" element-loading-text="正在加载..." stripe style="height:100%;"  border>
-    <el-table-column prop="customerName" label="供应商名称" sortable>
+    <el-table-column prop="projectTarget" label="项目代号" sortable>
     </el-table-column>
-    <el-table-column prop="contacts" label="联系人" sortable>
+    <el-table-column prop="customerName" label="客户名称" sortable>
     </el-table-column>
-    <el-table-column prop="phone" label="联系电话">
+    <el-table-column prop="contacts" label="联系人">
     </el-table-column>
-    <el-table-column prop="address" label="地址">
+    <el-table-column prop="tel" label="联系电话">
     </el-table-column>
-    <el-table-column prop="bankName" label="开户名">
+    <el-table-column prop="mail" label="邮箱地址">
     </el-table-column>
-    <el-table-column prop="creditCode" label="税号（统一社会信用代码）">
-    </el-table-column>
-    <el-table-column prop="bank" label="开户行">
-    </el-table-column>
-    <el-table-column prop="tel" label="电话">
-    </el-table-column>
-    <el-table-column prop="invoiceContent" label="发票内容">
-    </el-table-column>
-    <el-table-column prop="invoiceType" label="发票类型">
-    </el-table-column>
-    <el-table-column prop="account" label="账号">
-    </el-table-column>
-    <el-table-column prop="registerAddress" label="注册地址">
+    <el-table-column prop="connector" label="对接人">
     </el-table-column>
 </el-table>
 </template>
